a5fa47f04016bd54ac28ddc70feb2ab1c57ff22a
Robin Adrián <rarb@hotmail.com.ar>
2023-04-19 22:19:32 -0500

    Added app.js and otters elements to the index

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var a; 
+const b = "Hola"; // valor constante siempre constante
+let c; // variables usadas solamente dentro de un condicionalE
+
+$(document).ready(function(){
+    a = $("#numero_uno").val();//JQuery
+    b = document.getElementById("numero_dos").value;//javascript puro
+});
